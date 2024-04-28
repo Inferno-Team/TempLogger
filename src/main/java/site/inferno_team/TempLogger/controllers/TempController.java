5649275665d05d59e3a76468a884477324236ba5
Ahmad Mardenli <ahmad.m.200017@gmail.com>
2024-04-28 13:33:28 -0700

    Update temp id to be IDENTITY

--- src/main/java/site/inferno_team/TempLogger/controllers/TempController.java
+++ src/main/java/site/inferno_team/TempLogger/controllers/TempController.java
@@ -7,6 +7,7 @@ import site.inferno_team.TempLogger.repositories.TemperatureRepository;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/temp")
@@ -29,6 +30,15 @@ public class TempController {
     public String deleteTemp(int id){
         temperatureRepository.deleteById(id);
         return "deleted";
+    }
+    @PostMapping("/update")
+    public String updateTemp(@RequestBody Map<String, String> request){
+        Temperature temperature=temperatureRepository.findById(Integer.parseInt(request.get("id"))).orElse(null);
+        if(temperature!=null){
+            temperature.setTemperature(request.get("temperature"));
+            temperatureRepository.save(temperature);
+        }
+        return "updated";
     }
 
 }
